73588f229eba9b3ae137a4231df33e806482170f
nanaji1 <durgananaji85@gmail.com>
2022-11-23 14:46:51 +0530

    add the confirmation

--- day_3/script.js
+++ day_3/script.js
@@ -4,15 +4,20 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   let data = new FormData(form);
   data2 = Object.fromEntries(data.entries());
-  //   document.querySelector("#full_name").innerHTML = data2.full_name;
-  //   document.querySelector("#email").innerHTML = data2.email;
-  //   document.querySelector("#address").innerHTML = data2.address;
-  //   document.querySelector("#city").innerHTML = data2.city;
-  //   document.querySelector("#state").innerHTML = data2.state;
-  //   document.querySelector("#code").innerHTML = data2.pin_code;
   console.log(data);
   console.log(data2);
   for (const [name, value] of data) {
     console.log(name, ":", value);
   }
+  form.style = "display:none";
 });
+
+let popup = document.getElementById("pop_up");
+
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+function closePopup() {
+  popup.classList.remove("open-popup");
+  form.style = "display:block";
+}
